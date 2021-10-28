1c24928782fe78b24cff0bd84dc3c2a80ea4ce71
friilancer <jirapzo7@gmail.com>
2021-10-28 02:55:25 +0100

    fix: Short URIs are now generated using nanoid

--- controllers/shortenURL.js
+++ controllers/shortenURL.js
@@ -1,27 +1,8 @@
-const Uri = require('../model/uris');
-
-const generateShortUri = () => {
-	const arr = [];
-
-	for(let i=0; i < 6 ; i++){
-		let str = Math.floor(Math.random() * 26 ) + 97;
-		arr.push(String.fromCharCode(str)); 
-	}
-
-	return arr.join(''); 
-}
+const {nanoid} = require('nanoid')
 
 const createShortUri = async() => {
-	let shortened_uri = generateShortUri();
-	const link = await Uri.findOne({
-		where : {
-			shortened_uri
-		}
-	})
-
-	console.log(shortened_uri);
-
-	return link == null ? shortened_uri : createShortUri() ;
+	let id = await nanoid(9)
+	return id
 }
 
 module.exports = createShortUri;
